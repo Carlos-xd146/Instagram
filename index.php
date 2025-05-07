6398c505f4b4b00cd72c09edb023f6de0bcdb2ef
Carlos Eduardo <carloseduardodudu558@hotmail.com>
2025-05-06 21:00:10 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,38 +1,60 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/logo.png">
+    <link rel="stylesheet" href="./css/estilos.css">
+    <title>Instagram</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq"
+        crossorigin="anonymous"></script>
 
-$nome = "Carlos";
-$sobrenome = "Eduardo";
+</head>
 
-echo $nome . ' ' . $sobrenome;
+<body>
 
-echo '<br>';
+    <div class="container-fluid login justify-content-center d-flex align-items-center vh-100 flex-column">
+        <figure class="figure">
+            <img src="img/instagram.png" class="figure-img img-fluid">
+        </figure>
+        <input type="email" placeholder="email" class="mb-2 form-control">
+        <input type="password" placeholder="senha" class="mb-2 form-control">
+        <button class="btn btn-primary container-fluid">Entrar</button>
 
+        <p>OU</p>
 
-$nota = 11;
-if($nota<6){
-    echo "Nota: ". $nota . ' - ' . "REPROVADO!";
-} else {
-    echo "Nota: ". $nota . ' - ' . "APROVADO!";
-}
+        <a href="">Esqueceu a senha?</a>
+        <a href="">Entrar com o Google</a>
+        <p>Nao tem uma conta? <a href="">Cadastre-se</a></p>
 
+    </div>
+    <div>
+        <footer class="container-fluid text-cinza">
+            <div class="container-fluid d-flex justify-content-around flex-wrap gap-1">
+                <a href="" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover">Meta</a>
+                <a href="" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover">Sobre</a>
+                <a href="" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover">Blog</a>
+                <a href="" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover">Carreiras</a>
+                <a href="" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover">Ajuda</a>
+                <a href="" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover">API</a>
+                <a href="" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover">Privacidade</a>
+                <a href="" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover">Termos</a>
+                <a href="" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover">Localizações</a>
+                <a href="" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover">Instagram Lite</a>
+                <a href="" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover">Threads</a>
+                <a href="" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover">Upload de contatos e não usuários</a>
+                <a href="" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover">Meta Verified</a>
+            </div>
 
+            <h6>2025 HeroGeek Carlos Eduardo</h6>
+        </footer>
+        
+    </div>
 
+</body>
 
-// --------------------------
-
-
-$NomeServidor = "localhostt";
-$NomeUsuario = "root";
-$Senha = "";
-
-$conexao = new mysqli($NomeServidor, $NomeUsuario, $Senha);
-
-if($conexao->connect_error){
-    die("Conexão falhou: " . $conexao->connect_error);
-}
-
-echo '<br><br>';
-echo 'OK!';
-
-?>
+</html>
