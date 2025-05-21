b43190bc08fabe42ba49d5ddd4340875cfee6d42
Carlos Eduardo <carloseduardodudu558@hotmail.com>
2025-05-20 21:04:39 -0300

    pagina principal

---
+++ home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ept-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina Principal</title>
+    <link rel="stylesheet" href="./css/home.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+</head>
+
+<body>
+
+   <div >
+  
+  </div>
+</div>
+</body>
+
+</html>
